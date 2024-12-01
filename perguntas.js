1b92507067c86a80d230562f97dc044ef3ce0771
joaomedeiros13 <joaovicmed1303@gmail.com>
2024-11-30 21:56:31 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Esporte',
+    'Quem é Neymar?',
+    'Neymar é um jogador de futebol'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital de Portugual?',
+    'A capital da Portugual é Lisboa'
+)
+
+criaCartao(
+    'Esporte',
+    'Quantas pessoas jogam em um campo de futebol?',
+    'Em um campo de futebol tem 11 jogadores'
+)
+
+criaCartao(
+    'Esporte',
+    'Quantas copas do mundo tem o Brasil?',
+    'O Brasil tem 5 copas'
+)
